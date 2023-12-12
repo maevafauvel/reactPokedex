71af4f173ce1c1954ca16c1b2537fae569cedc19
Maeva Fauvel <mae.fauvel@gmail.com>
2023-12-12 13:51:21 +0100

    Step 3 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,7 +1,20 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    const pokemon = pokemonList[0];
+
     return <figure>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-        <figcaption>Bulbasaur</figcaption>
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+        <figcaption>{pokemon.name}</figcaption>
     </figure>;
 }
 
